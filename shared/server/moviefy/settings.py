806a322ab323edfc0decee7f6c229a7af4ce9cb6
conrad123 <outerhaven@live.it>
2016-05-12 11:36:02 +0200

    templates directory moved

--- shared/server/moviefy/settings.py
+++ shared/server/moviefy/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'moviefy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'moviefy/app/templates')],
+        'DIRS': [os.path.join(BASE_DIR,'moviefy/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

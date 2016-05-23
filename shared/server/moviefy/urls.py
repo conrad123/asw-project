555b092eeeee54bd5ebab336d7658b7bb0c58970
Lelouch21 <fabio.raschini@gmail.com>
2016-05-23 19:57:45 +0200

    Resolve the errors on urls!!

--- shared/server/moviefy/urls.py
+++ shared/server/moviefy/urls.py
@@ -4,10 +4,8 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^gestione-film/', 'moviefy.views.gestione_film_handler'),
     url(r'^inserisci-film/', 'moviefy.views.inserisci_film_handler'),
     url(r'^salva-film/', 'moviefy.views.salva_film_handler'),
-    url(r'^gestione-generi/', 'moviefy.views.gestione_generi_handler'),
     url(r'^inserisci-genere/', 'moviefy.views.inserisci_genere_handler'),
     url(r'^salva-genere/', 'moviefy.views.salva_genere_handler'),
     url(r'^catalogo-film/', 'moviefy.views.catalogo_film_handler'),

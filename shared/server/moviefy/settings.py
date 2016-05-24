0f2938b164d567682da84507f3d5ccb3b84fc45b
Fabio <fabio.raschini@gmail.com>
2016-05-24 17:03:26 +0200

    Unable Rest with curl, authentication rest removed

--- shared/server/moviefy/settings.py
+++ shared/server/moviefy/settings.py
@@ -61,11 +61,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'moviefy.wsgi.application'
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
 }
 
 # Database
